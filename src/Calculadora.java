ba79b01bdf304a8fe0c686f79783aadca180cbaa
lucasemanoel3103 <160548558+lucasemanoel3103@users.noreply.github.com>
2024-07-10 21:20:02 -0300

    projeto de estudo de calculadora em java

---
+++ src/Calculadora.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void main(String[] args) throws Exception {
+       Scanner scanner = new Scanner(System.in);
+       System.out.println("Digite o primeiro número: ");
+       double num1 = scanner.nextDouble();
+       System.out.println("Digite o segundo número: ");
+       double num2 = scanner.nextDouble();
+       System.out.println("Digite a operação (+, -, *, /): ");
+       char operator = scanner.next().charAt(0);
+       scanner.close();
+       double result;
+
+       switch(operator){
+         case '+':
+            result = num1 + num2;
+            break;
+         case '-':
+            result = num1 - num2;
+            break;
+         case '*':
+            result = num1 * num2;
+            break;
+         case '/':
+            result = num1 / num2;
+            break;
+         default:
+            System.out.println("Operação inválida.");
+            return;
+
+         
+       }
+
+        System.out.println(num1+" "+operator+" "+num2+"= "+result);
+
+    }
+}
